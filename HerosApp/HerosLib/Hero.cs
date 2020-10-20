cbacc6de64b6d25dc0c8b20e34aaf6d7b22cdbc0
Julie <julie.alex.reyes@gmail.com>
2020-10-20 13:34:17 -0500

    HerosApp created and updated

---
+++ HerosApp/HerosLib/Hero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HerosLib
+{
+    public class Hero
+    {
+        public int id;         // value type => System.Int32
+        public string name;    // reference type
+
+        //constructor
+        public Hero()
+        {
+            id = 1;
+            name = "Bombasto";
+        }
+    }
+
+}
---
+++ HerosApp/HerosUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HerosLib;
+
+namespace HerosUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Hero obj = new Hero();
+            Console.WriteLine($"{obj.id}, {obj.name}");
+        }
+    }
+}
